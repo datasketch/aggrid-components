d5c21b59c71b8831ac2d9d6c6368c88ffbeb6755
Luis Calderon <luixcalderon12@gmail.com>
2024-09-18 15:36:50 -0500

    v0.1.12: Export components colors to use

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Custom editors and renderers for [aggrid](https://www.ag-grid.com/).
 ### Editors
 
 - [CategoryCellEditor](src/editors/CategoryCellEditor.tsx)
-
+- [ColorCellEditor](src/editors/ColorCellEditor.tsx)
 ### Renderers
 
 - [CategoryCellRenderer](src/renderers/CategoryCellRenderer.tsx)
 - [EmailCellRenderer](src/renderers/EmailCellRenderer.tsx)
 - [ImageCellRenderer](src/renderers/ImageCellRenderer.tsx)
 - [URLCellRenderer](src/renderers/URLCellRenderer.tsx)
+- [ColorCellRenderer](src/renderers/ColorCellRenderer.tsx)
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import EmailCellRenderer from "./renderers/EmailCellRenderer";
 import URLCellRenderer from "./renderers/URLCellRenderer";
 import ImageCellRenderer from "./renderers/ImageCellRenderer";
 import CategoryCellRenderer from "./renderers/CategoryCellRenderer";
+import ColorCellRenderer from "./renderers/ColorCellRenderer";
+import ColorCellEditor from "./editors/ColorCellEditor";
 import CategoryCellEditor, { suppressKeyboardEvent } from "./editors/CategoryCellEditor";
 
-export { EmailCellRenderer, URLCellRenderer, ImageCellRenderer, CategoryCellRenderer, CategoryCellEditor, suppressKeyboardEvent }
+export { EmailCellRenderer, URLCellRenderer, ImageCellRenderer, CategoryCellRenderer, CategoryCellEditor, suppressKeyboardEvent, ColorCellRenderer, ColorCellEditor }
